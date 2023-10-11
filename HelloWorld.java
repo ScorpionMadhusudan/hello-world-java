833bbfe71122bf057464355fe24788345c5ab0bc
ScorpionMadhusudan <navani.madhsudan@gmail.com>
2023-10-11 06:24:08 +0530

    modififed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world! Madhusudan here a Devops Expert");
     System.out.println("Hello world! Madhusudan here a Devops Expert");
-    System.out.println("Hello world! Madhusudan here a Devops Expert");
+    System.out.println("Hello world! Madhusudan here a Devops Expert Awesomeeeeeeeeeeeeee");
     System.out.println("Hello world! Madhusudan here a Devops Expert from Bangalore, Jenkins webhook practice.. Coooooooooooooooooooooool ");
   }
 }
