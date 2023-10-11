2fc934eef65c563516efa4d4293e7da855d726b3
ScorpionMadhusudan <140151988+ScorpionMadhusudan@users.noreply.github.com>
2023-10-11 05:34:29 +0530

    Update HelloWorld.java
    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! Madhusudan here");
+    System.out.println("Hello world! Madhusudan here a Devops Expert");
   }
 }
