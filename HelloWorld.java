75d15cb91b569f8f5370924384e74f80f2e69728
ScorpionMadhusudan <navani.madhsudan@gmail.com>
2023-10-11 08:04:04 +0530

    merge conflict fixed

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,11 @@ public class HelloWorld {
     System.out.println("Hello world! Madhusudan here a Devops Expert");
     System.out.println("Hello world! Madhusudan here a Devops Expert");
     System.out.println("Hello world! Madhusudan here a Devops Expert Awesomeeeeeeeeeeeeee");
-<<<<<<< HEAD
  System.out.println("Hello world! Madhusudan here a Devops Expert from Bangalore, Jenkins webhook practice.. Coooooooooooooooooooooool.....On Local Windows Machine madhuuuuuuuuuuuuuuuuuuuuuuuuuu ");
 System.out.println("Hello world! Madhusudan here a Devops Expert from Bangalore, Jenkins webhook practice.. Coooooooooooooooooooooool.....On Local Windows Machine madhuuuuuuuuuuuuuuuuuuuuuuuuuu Sudannnnnnnnnnnnnnnnnnnnnnnnnn ");
  System.out.println("Hello world! Madhusudan here a Devops Expert from Bangalore, Jenkins webhook practice.. Coooooooooooooooooooooool.....On Local Windows Machine madhuuuuuuuuuuuuuuuuuuuuuuuuuu ");
-=======
     System.out.println("Hello world! Madhusudan here a Devops Expert from Bangalore, Jenkins webhook practice.. Coooooooooooooooooooooool.....On Local Windows Machine madhuuuuuuuuuuuuuuuuuuuuuuuuuu ");
    System.out.println("Hello world! Madhusudan here a Devops Expert Awesomeeeeeeeeeeeeee");
     ystem.out.println("Hello world! Madhusudan here a Devops Expert from Bangalore, Jenkins webhook practice.. Coooooooooooooooooooooool.....On Local Windows Machine madhuuuuuuuuuuuuuuuuuuuuuuuuuu ");
->>>>>>> 565a76e3586685848add687a0bf569e35ab280e8
   }
 }
